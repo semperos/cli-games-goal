52f482e6269661c8f0966cce167f218d854988f5
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2025-10-17 16:36:21 -0400

    In the beginning was the Word

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/semperos/goal-for-cli-games
+
+go 1.25.1
+
+require (
+	codeberg.org/anaseto/goal v1.4.0 // indirect
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"codeberg.org/anaseto/goal"
+	gos "codeberg.org/anaseto/goal/os"
+	"github.com/eiannone/keyboard"
+)
+
+//go:embed k.goal
+var kGoalContents string
+
+func main() {
+	var gameFile string
+	flag.StringVar(&gameFile, "game", "", "Game file to load")
+	flag.StringVar(&gameFile, "g", "", "Game file to load (shorthand)")
+	flag.Parse()
+
+	if gameFile == "" {
+		fmt.Println("Please provide a file name with --game or -g")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	contents, err := os.ReadFile(gameFile)
+	if err != nil {
+		fmt.Printf("Error reading file %s: %v\n", gameFile, err)
+		os.Exit(1)
+	}
+	gameGoalSource := string(contents)
+
+	fmt.Printf("Loading game file: %s\n", gameFile)
+	ctx := goal.NewContext()
+	ctx.Log = os.Stderr
+	gos.Import(ctx, "")
+
+	_, err = ctx.EvalPackage(kGoalContents, "<builtin>", "k")
+	if err != nil {
+		fmt.Printf("Error loading k.goal: %v\n", err)
+	}
+
+	_, err = ctx.Eval(gameGoalSource)
+	if err != nil {
+		fmt.Printf("Error evaluating Goal game source from file %s: %v\n", gameFile, err)
+	}
+
+	if err := keyboard.Open(); err != nil {
+		fmt.Println("Failed to open keyboard:", err)
+		return
+	}
+	defer keyboard.Close()
+
+	tick := time.NewTicker(100 * time.Millisecond)
+	defer tick.Stop()
+
+	keyEvents, err := keyboard.GetKeys(10)
+	if err != nil {
+		fmt.Println("Failed to get keys:", err)
+		return
+	}
+
+	_, err = ctx.Eval("render[board]")
+	if err != nil {
+		fmt.Printf("Error rendering initial board: %v\n", err)
+	}
+	for {
+		select {
+		case <-tick.C:
+		case event := <-keyEvents:
+			if event.Err != nil {
+				fmt.Println("Keyboard error:", event.Err)
+				return
+			}
+			if event.Rune == 'q' {
+				fmt.Println("Bye!")
+				os.Exit(0)
+			}
+			switch event.Key {
+			case keyboard.KeyArrowUp:
+				_, err = ctx.Eval(update("n"))
+				if err != nil {
+					fmt.Printf("Error moving n: %v\n", err)
+				}
+			case keyboard.KeyArrowDown:
+				_, err = ctx.Eval(update("s"))
+				if err != nil {
+					fmt.Printf("Error moving s: %v\n", err)
+				}
+			case keyboard.KeyArrowLeft:
+				_, err = ctx.Eval(update("w"))
+				if err != nil {
+					fmt.Printf("Error moving w: %v\n", err)
+				}
+			case keyboard.KeyArrowRight:
+				_, err = ctx.Eval(update("e"))
+				if err != nil {
+					fmt.Printf("Error moving e: %v\n", err)
+				}
+			case keyboard.KeyEnter:
+				_, err = ctx.Eval(`reset""; render[board]`)
+				if err != nil {
+					fmt.Printf("Error resetting board: %v\n", err)
+				}
+			case keyboard.KeyEsc:
+				fmt.Println("Later!")
+				os.Exit(0)
+			}
+		}
+	}
+}
+
+func update(dir string) string {
+	return fmt.Sprintf("next[board;\"%s\"]; render[board]", dir)
+}
